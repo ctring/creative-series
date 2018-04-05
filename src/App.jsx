44d5c603416bd151007f0a74dcf4b95865aa6533
Cuong Nguyen <ctring23@gmail.com>
2018-04-05 03:55:17 -0400

    Set up layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Container, Header, Button, Grid } from 'semantic-ui-react';
 import TimeSeriesDrawer from './components/TimeSeriesDrawer.jsx';
 
 class App extends Component {
@@ -7,7 +8,25 @@ class App extends Component {
     let series = [1, 20, 30, 15, 2, 3, 12, 3.2, 32.4];
     return (
       <div className="App">
-        <TimeSeriesDrawer width={500} height={300} series={series}/>
+        <Header style={{marginTop: '20px'}} as='h1'textAlign='center'>
+          Time Series Drawer
+        </Header>
+        <Container style={{marginTop: '20px'}}>
+          <TimeSeriesDrawer width={1200} height={300} series={series}/>
+        </Container>
+        <Grid container divided='horizontally'>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <div>
+                <textarea cols={72} rows={6} style={{resize: 'none'}}
+                  placeholder='Enter a series of number, separated by commas'/>
+              </div>
+              <Button>Set Time Series</Button>
+            </Grid.Column>
+            <Grid.Column>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     );
   }
