8e76ff4d7cf0fbeb94b412f255ee6cf354875f1f
Cuong Nguyen <ctring23@gmail.com>
2018-04-07 02:39:49 -0400

    Refactor TimeSeriesDrawer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Container, Header, Button, Grid } from 'semantic-ui-react';
 import TimeSeriesDrawer from './components/TimeSeriesDrawer.jsx';
 
 class App extends Component {
+
   render() {
     let series = [1, 20, 30, 15, 2, 3, 12, 3.2, 32.4];
     return (
@@ -12,9 +13,9 @@ class App extends Component {
           Time Series Drawer
         </Header>
         <Container style={{marginTop: '20px'}}>
-          <TimeSeriesDrawer width={1200} height={300} series={series}/>
+          <TimeSeriesDrawer width={1200} height={500} length={50}/>
         </Container>
-        <Grid container divided='horizontally'>
+        <Grid container divided>
           <Grid.Row columns={2}>
             <Grid.Column>
               <div>
@@ -24,6 +25,7 @@ class App extends Component {
               <Button>Set Time Series</Button>
             </Grid.Column>
             <Grid.Column>
+
             </Grid.Column>
           </Grid.Row>
         </Grid>
--- src/components/TimeSeriesDrawer.jsx
+++ src/components/TimeSeriesDrawer.jsx
@@ -2,74 +2,102 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Stage, Layer, Line, Circle } from 'react-konva';
 
+function scale(x, fromLow, fromHigh, toLow, toHigh) {
+  let factor = (toHigh - toLow) / (fromHigh - fromLow);
+  return factor * (x - fromLow) + toLow;
+}
+
 export default class TimeSeriesDrawer extends Component {
 
   state = {
-    offset: [] 
+    userOffset: [],
+    screenOffset: [],
+  };
+
+  padding = 10;
+
+  // Series in user coordinate system
+  userSeries() {
+    let length = this.props.length || 10;
+    return this.props.series || Array(length).fill(0);
   }
 
-  padding = 10
-
-  middleY() {
-    return this.props.height / 2;
+  // Y scale from user
+  userScaleY() {
+    return this.props.scaleY || [-1, 1];
   }
 
-  length() {
-    return this.props.length || 5;
-  }
-
-  ys() {
-    return this.props.series || Array(this.length()).fill(0);
+  // Series in screen coordinate system
+  screenSeries() {
+    let userSeries = this.userSeries();
+    let userScaleY = this.userScaleY();
+    return userSeries.map((y, i) => (
+      scale(y, userScaleY[0], userScaleY[1], this.props.height, 0)
+    ));
   }
 
   space() {
-    let { width } = this.props; 
-    return (width - 2 * this.padding) / (this.length() - 1);
+    let userSeries = this.userSeries();
+    return (this.props.width - 2 * this.padding) / (userSeries.length - 1);
+  }
+
+  // X-coordinate in screen coordinate system
+  screenX() {
+    let userSeries = this.userSeries();
+    let space = this.space();
+    return userSeries.map((_, i) => (
+      this.padding + i * space
+    ));
   }
 
   renderLines() {
-    let { offset } = this.state;
+    let { screenOffset } = this.state;
+    let screenSeries = this.screenSeries();
+    let screenX = this.screenX();
     var points = [];
-    let space = this.space();
-    this.ys().forEach((y, i) => {
-      points.push(this.padding + i * space);
-      points.push(this.middleY() - y + (offset[i] || 0));
+    screenSeries.forEach((y, i) => {
+      points.push(screenX[i] + this.padding);
+      points.push(y + (screenOffset[i] || 0));
     });
     return <Line points={points} stroke='red' />
   }
 
   renderPoints() {
     let { pointRadius } = this.props;
-    let space = this.space();
-    return this.ys().map((y, i) => {
-      let { offset } = this.state;
-      let trueY = this.middleY() - y;
-      return <Circle 
-        x={this.padding + i * space}
-        y={trueY + (offset[i] || 0)}
-        radius={pointRadius || 5}
+    let { screenOffset } = this.state;
+    let screenSeries = this.screenSeries();
+    let screenX = this.screenX();
+    return screenSeries.map((y, i) => (
+      <Circle
+        x={screenX[i] + this.padding}
+        y={y + (screenOffset[i] || 0)}
+        radius={pointRadius || 2}
         fill='black'
-        key={i}
+        key={'ts-' + i}
       />
-    })
+    ))
   }
 
   render() {
     let { width, height } = this.props;
-    let { offset } = this.state;
+    let { screenOffset, userOffset } = this.state;
 
     let space = this.space();
-    let middleY = this.middleY();
-    let ys = this.ys();
+    let screenSeries = this.screenSeries();
+    let userScaleY = this.userScaleY();
 
+    // Function for updating the offsets of the points
     let offsetUpdateFunc = (e) => {
       if (e.evt && e.evt.buttons === 1) {
-        let index = Math.floor((e.evt.layerX - this.padding)/space);
-        var newOffset = offset.slice();
-        let newY = e.evt.layerY;
-        let trueY = middleY - ys[index];
-        newOffset[index] = newY - trueY;
-        this.setState({ 'offset': newOffset});
+        let index = Math.floor((e.evt.layerX - this.padding) / space);
+
+        var newScreenOffset = screenOffset.slice();
+        var newUserOffset = userOffset.slice();
+
+        newScreenOffset[index] = e.evt.layerY - screenSeries[index];
+        newUserOffset[index] = scale(newScreenOffset[index], 0, width, userScaleY[1], userScaleY[0]);
+
+        this.setState({ screenOffset: newScreenOffset, userOffset: newUserOffset });
       }
     };
 
@@ -91,5 +119,6 @@ TimeSeriesDrawer.propTypes = {
   height: PropTypes.number.isRequired,
   length: PropTypes.number,
   series: PropTypes.array,
+  scaleY: PropTypes.array,
   pointRadius: PropTypes.number
 }
