8b1002aacababa2c0993c8216a2e8c99c8f1dfad
Cuong Nguyen <ctring23@gmail.com>
2018-04-07 20:10:43 -0400

    Button to copy output to clipboard

--- src/components/CopiableTextOutput.jsx
+++ src/components/CopiableTextOutput.jsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Container } from 'semantic-ui-react';
+import { Container, Button, Segment, Message } from 'semantic-ui-react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
-export default function CopiableTextOutput(props) {
-  return (
-    <Container fluid>
-      <p>{props.content}</p>
-    </Container>
-  );
+class CopiableTextOutput extends Component {
+
+  state = {
+    copied: false,
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { copied: false };
+  }
+
+  render() {
+    const { copied } = this.state;
+    return (
+      <Container fluid>
+        <Segment attached='top'>
+          {this.props.content}
+        </Segment>
+
+        <CopyToClipboard text={this.props.content}>
+          <Button
+            attached='bottom'
+            icon='clipboard'
+            content='Copy to clipboard'
+            onClick={() => { this.setState({ copied: true }) }} />
+        </CopyToClipboard>
+
+        <Message positive hidden={!copied}>
+          Copied to clipboard!
+        </Message>
+      </Container >
+    )
+  };
 }
 
 CopiableTextOutput.propTypes = {
   content: PropTypes.string.isRequired,
 };
+
+export default CopiableTextOutput;
